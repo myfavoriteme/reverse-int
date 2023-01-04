e50235867c1514600f3978b52b201a2ac686e18b
Vladislav Kakorin <vlad.kakorin03@gmail.com>
2023-01-05 00:33:25 +0300

    feat: Node was installed. Task was solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
+  let out = '';
   
+  let str = (Math.abs(n)).toString();
+  let len = n.length;
+
+  for (let i = 0; i < str.length; i++) {
+      out += str[str.length - i - 1];
+    }
+
+  out = out * 1;
+  return out;
 }
